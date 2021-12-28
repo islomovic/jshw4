56e243a1a37ee886c5475745839b3109b318cb05
islomovic <92381015+islomovic@users.noreply.github.com>
2021-12-28 18:11:41 +0500

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+
+let username = "admin"
+let a = prompt("login");
+if (a === username){
+  console.log(a);
+}
+else {
+  alert("bomadi");
+}
+
+let password = "12345"
+let b = prompt("password")
+if (b === password){
+  console.log(b);
+  alert(" salom ");
+}
